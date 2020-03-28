b0aa1316069cb83cdd98ea596ddb23c450854053
keplor <keplor@naver.com>
2020-03-28 15:41:36 +0900

    refactor solution method

--- src/main/java/com/kakao/v2020/mocktest/p4/Solution.java
+++ src/main/java/com/kakao/v2020/mocktest/p4/Solution.java
@@ -1,7 +1,7 @@
 package com.kakao.v2020.mocktest.p4;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 카카오 2020 인턴 모의고사 : 문제 4번
@@ -10,31 +10,58 @@ import java.util.Set;
  * @since 20.03.28
  */
 public class Solution {
-    static Set<Long> roomAssignment;
+    static RoomInfo roomInfo;
+    static long[] result;
+    static long roomCnt;
 
     public static long[] solution(long k, long[] room_number) {
-        roomAssignment = new HashSet<>();
-        long[] guestRoomInfo = new long[room_number.length];
+        roomInfo = new RoomInfo();
+        result = new long[room_number.length];
+        roomCnt = k;
 
         for (int guestNo = 0; guestNo < room_number.length; guestNo++) {
             long wantedRoomNo = room_number[guestNo];
+            long nextRoomNo = wantedRoomNo + roomInfo.getRoomCount(wantedRoomNo);
 
-            if (!roomAssignment.contains(wantedRoomNo)) {
-                // 1.1 손님이 원하는 방이 비어 있다면
-                roomAssignment.add(wantedRoomNo);
-                guestRoomInfo[guestNo] = wantedRoomNo;
-            } else {
-                // 1.2 이미 투숙중이면
-                for (long otherRoomNo = wantedRoomNo + 1; otherRoomNo <= k; otherRoomNo++) {
-                    if (!roomAssignment.contains(otherRoomNo)) {
-                        roomAssignment.add(otherRoomNo);
-                        guestRoomInfo[guestNo] = otherRoomNo;
-                        break;
-                    }
-                }
-            }
+            assignRoom(guestNo, nextRoomNo);
         }
 
-        return guestRoomInfo;
+        return result;
+    }
+
+    private static void assignRoom(int guestNo, long nextRoomNo) {
+        for (long roomNo = nextRoomNo; roomNo <= roomCnt; roomNo++) {
+            if (roomInfo.isEmptyRoom(roomNo)) {
+                roomInfo.assign(roomNo);
+                result[guestNo] = roomNo;
+                break;
+            }
+            roomInfo.updateRoomInfo(roomNo);
+        }
+    }
+
+    static class RoomInfo {
+        private Map<Long, Integer> roomAssignment;
+
+        public RoomInfo() {
+            this.roomAssignment = new HashMap<>();
+        }
+
+        public boolean isEmptyRoom(long roomNo) {
+            return !roomAssignment.containsKey(roomNo);
+        }
+
+        public void assign(long roomNo) {
+            roomAssignment.put(roomNo, 1);
+        }
+
+        public void updateRoomInfo(long roomNo) {
+            roomAssignment.put(roomNo, roomAssignment.get(roomNo) + 1);
+        }
+
+        public int getRoomCount(long roomNo) {
+            return (roomAssignment.get(roomNo) == null) ? 0
+                    : roomAssignment.get(roomNo);
+        }
     }
 }
--- src/test/java/com/kakao/v2020/mocktest/p4/SolutionTest.java
+++ src/test/java/com/kakao/v2020/mocktest/p4/SolutionTest.java
@@ -4,9 +4,6 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @author 송훤출
  * @since 20.03.28
@@ -19,25 +16,5 @@ public class SolutionTest {
         long k = 10;
 
         Assert.assertThat(new long[]{1L, 3L, 4L, 2L, 5L, 6L}, CoreMatchers.is(Solution.solution(k, room_number)));
-    }
-
-    @Test
-    public void ㄴㅇ() {
-        Set<Long> set = new HashSet<>();
-
-        long a = 1L;
-        set.add(a);
-        set.add(3L);
-        for (long i = a; i < 20; i++) {
-            if (!set.contains(i)) {
-                set.add(i);
-                break;
-            }
-        }
-
-        boolean result = set.contains(2L);
-        Assert.assertThat(true, CoreMatchers.is(result));
-
-
     }
 }
